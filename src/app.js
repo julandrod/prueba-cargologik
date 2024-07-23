8b7a9a8838bc5c2ab887719a7919ec790b977dfc
julandrod <julian.andres.rodriguez@gmail.com>
2024-07-23 14:09:05 -0500

    add logic necessary for the swagger documentation

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ import { connectDB } from "./db/connect.js";
 import ventasRouter from "./routes/ventas.routes.js";
 import { notFoundMiddleware } from "./middlewares/notFound.middleware.js";
 import { errorMiddleware } from "./middlewares/errors.middleware.js";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { options } from "./swagger-doc/options.js";
+
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -12,6 +16,10 @@ app.get("/api/v1", (req, res) => {
 });
 
 app.use("/api/v1/ventas", ventasRouter)
+
+// Ruta para la documentacion de la API en swagger
+const specs = swaggerJsdoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use(notFoundMiddleware)
 app.use(errorMiddleware)
