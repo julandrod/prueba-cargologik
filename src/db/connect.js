f31e3f711036210d500ae71dadae3701ee797287
julandrod <julian.andres.rodriguez@gmail.com>
2024-07-23 13:26:46 -0500

    add documentation to db connect

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,5 +1,11 @@
 import mongoose from "mongoose";
 
+/**
+ * Gestiona la conexion a la base de datos de mongo DB
+ * 
+ * @param {string} url url de mongo para conectarse a la base de datos
+ * @returns mensaje de conexion exitosa o mensaje de error
+ */
 export const connectDB = async (url) => {
   return mongoose
     .connect(url)
